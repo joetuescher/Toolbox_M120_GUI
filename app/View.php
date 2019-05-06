5add42db914e1580ed6b5dcdd445af60d38d9570
Joe Tüscher <joe01tuescher@gmail.com>
2019-05-06 14:22:56 +0200

    Update View.php

--- app/View.php
+++ app/View.php
@@ -6,7 +6,7 @@ class View{
 	}
 	public function showNav(){
 		echo '<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">Navigation</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -34,11 +34,9 @@ class View{
         <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
-</nav>';
+</nav>
+
+';
 	}
 }
