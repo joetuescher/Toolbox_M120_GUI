a311aba4c01a97b3b0263336fd69b54c84854f6c
Joe Tüscher <joe01tuescher@gmail.com>
2019-05-06 14:49:56 +0200

    Update View.php

--- app/View.php
+++ app/View.php
@@ -6,26 +6,23 @@ class View{
 	}
 	public function showNav(){
 		echo '<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navigation</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <form>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <button type="submit" name="Action" class="btn" value="Home">Home</button>
-      </li>
-      <li class="nav-item">
-        <button type="submit" name="Action" class="btn" value="Hash">Hash Generator</button>
-      </li>
-      <li class="nav-item">
-        <button type="submit" name="Action" class="btn" value="huren">Joel</button>
-      </li>
-    </ul>
-  </div>
-</form>
-</nav>';
+            <a class="navbar-brand" href="#">Navigation</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <form>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                  <button type="submit" name="Action" class="btn" value="Home">Home</button>
+                </li>
+                <li class="nav-item">
+                  <button type="submit" name="Action" class="btn" value="Hash">Hash Generator</button>
+                </li>
+              </ul>
+            </div>
+          </form>
+          </nav>';
 	}
   public function showHome(){
     echo '<h1>Home</h1>';
