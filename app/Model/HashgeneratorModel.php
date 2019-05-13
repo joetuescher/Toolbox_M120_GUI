f871519a079a910007b5804f3fe47d78cb74ad8e
federico426 <48211384+federico426@users.noreply.github.com>
2019-05-13 13:36:15 +0200

    dqew

--- app/Model/HashgeneratorModel.php
+++ app/Model/HashgeneratorModel.php
@@ -2,7 +2,12 @@
 
 class Model{
 
-	public function __construct(){
-		
+	public function __construct(){}
+
+	public function hashGenerator($value){
+
+		$hashKey = sha1(md5($value));
+
+		return $hashKey;
 	}
 }
